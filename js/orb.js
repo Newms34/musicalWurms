64525b574307ae60fed5c9c17c1cae1b6376c06f
newms34 <newms3450@gmail.com>
2019-09-10 14:48:36 -0400

    batch committing everything

--- js/orb.js
+++ js/orb.js
@@ -1,152 +1,142 @@
-var canv = document.querySelector('#myCanv'),
-    ctx = canv.getContext("2d"),
-    orgCon = function(a) {
-        this.x = Math.floor(Math.random() * ($(window).width() - 3));
-        this.y = Math.floor(Math.random() * ($(window).height() - 3));
-        this.vx = (Math.random() * 4) - 2;
-        this.vy = (Math.random() * 4) - 2;
-        this.mass = Math.floor(Math.random() * 12) + 1;
-        this.targ={x:0,y:0};
-    };
-//setup canvas;
-canv.width = $(window).width();
-canv.height = $(window).height();
-canv.style.width = canv.width + 'px';
-canv.style.height = canv.height + 'px';
+//pointers are pointing in the wrong direction. is this an error with the accel calcs or an error with the pointer calcs?
 
-//for some reason, vx,vy do not seem to be decreasing!
-
-var app = angular.module('orbApp', []).controller('orbCon', function($scope) {
+var app = angular.module('gravSim', []).controller('gravCont', function($scope) {
     $scope.parts = [];
-    $scope.maxDist = 70;
-    $scope.glo = true;
-    $scope.numParts = 6;
-    $scope.trailMode = false;
-    $scope.bigG = .1;
-    $scope.customHue = 0;
-    $scope.customHueMode = false;
-    $scope.showVecs=true;
-    $scope.mousePos = {x:0,y:0};
-    //monitors
-    $scope.cw = canv.width;
-    $scope.ch = canv.height;
-    //end monitors
+    $scope.numParts = 10;
+    $scope.bigG = 5;
+    $scope.field = {
+        x: $(window).width(),
+        y: $(window).height()
+    }
+    $scope.elapsed = 0;
+    $scope.canv = document.querySelector('#canv');
+    $scope.ctx = $scope.canv.getContext('2d');
+    $scope.canv.width = $scope.field.x;
+    $scope.canv.height = $scope.field.y;
+    $scope.canv.style.width = $scope.field.x + 'px';
+    $scope.canv.style.height = $scope.field.y + 'px';
 
-    $scope.initParts = function() {
-        $scope.parts = [];
-        for (var i = 0; i < $scope.numParts; i++) {
-            $scope.parts.push(new orgCon())
+    $scope.partConst = function() {
+        this.x = Math.floor(Math.random() * $scope.field.x);
+        this.y = Math.floor(Math.random() * $scope.field.y);
+        this.mass = Math.floor(Math.random() * 10) + 2;
+        this.temp = {
+            h: 60,
+            v: 50
+        };
+        this.a = { x: 0, y: 0 }
+        this.vx = 0;
+        this.vy = 0;
+        this.data = '';
+    }
+    $scope.pointerAng = function(p) {
+        var ang = Math.atan(p.a.y / p.a.x) * 180 / Math.PI;
+        if (p.a.x > 0) {
+            ang += 180;
         }
+        return ang;
     };
-    $scope.toggleOpts = function() {
-        $('#swarmOpts').toggle(100);
+    $scope.initP = function() {
+        $scope.parts = []; //empty partz
+        for (var i = 0; i < $scope.numParts; i++) {
+            $scope.parts.push(new $scope.partConst());
+        }
+        $scope.elapsed = 0;
+        $scope.ctx.fillStyle = '#000';
+        $scope.ctx.fillRect(0, 0, $scope.canv.width, $scope.canv.height)
     }
-    $scope.initParts();
-    $scope.rescatter = function() {
-        $scope.parts.forEach((p) => {
-            p.x = Math.floor(Math.random() * (canv.width - 2));
-            p.y = Math.floor(Math.random() * (canv.height - 2));
-        })
-    }
-    ctx.strokeStyle = '#fff';
-    ctx.fillRect(0, 0, canv.width, canv.height);
+    $scope.initP();
     $scope.timer = setInterval(function() {
-        //first, clear canvas
-        ctx.fillStyle = '#000';
-        if (!$scope.trailMode) ctx.fillRect(0, 0, canv.width, canv.height); //if trail mode is off, we clear canvas each time.
-        $scope.parts.forEach(function(p, n) {
-            var closestDist = Math.sqrt(Math.pow(canv.width, 2) + Math.pow(canv.height, 2)),
-                prevPos = {
-                    x: p.x,
-                    y: p.y
-                };
-            var prevVal = { x: p.targ.x, y: p.targ.y }
-            p.targ.x = 0;
-            p.targ.y = 0;
-
-            // re-enable below for pp grav
-            for (var i = 0; i < $scope.parts.length; i++) {
-                if (i !== n) {
-                    var xMult = parseInt(p.x > $scope.parts[i].x ? -1 : 1);
-                    var yMult = parseInt(p.y > $scope.parts[i].y ? -1 : 1);
-                    if (p.x != $scope.parts[i].x && Math.abs(p.x-$scope.parts[i].x)>p.mass) {
-                        p.targ.x += xMult * ($scope.bigG * p.mass * $scope.parts[i].mass) / Math.pow(Math.abs(p.x - $scope.parts[i].x), 2);
-                    }
-                    if (p.y != $scope.parts[i].y && Math.abs(p.y-$scope.parts[i].y)>p.mass) {
-                        p.targ.y += yMult * ($scope.bigG * p.mass * $scope.parts[i].mass) / Math.pow(Math.abs(p.y - $scope.parts[i].y), 2);
-                    }
+        if (!$scope.trailMode) {
+            $scope.ctx.fillStyle = '#000';
+            $scope.ctx.fillRect(0, 0, $scope.canv.width, $scope.canv.height)
+        }
+        for (var i = 0; i < $scope.parts.length; i++) {
+            if ($scope.trailMode) {
+                //draw trails on canv;
+                $scope.ctx.fillStyle = 'rgba(0,0,0,.1)'
+                $scope.ctx.fillRect(($scope.parts[i].x + ($scope.parts[i].mass / 3)), ($scope.parts[i].y + ($scope.parts[i].mass / 3)), ($scope.parts[i].mass / 3), ($scope.parts[i].mass / 3))
+                $scope.ctx.fillStyle = 'hsla(' + 360 * (i / $scope.numParts) + ',100%,' + Math.floor($scope.parts[i].temp.v + 20) + '%,.1)'
+                $scope.ctx.fillRect(($scope.parts[i].x + ($scope.parts[i].mass / 2)), ($scope.parts[i].y + ($scope.parts[i].mass / 2)), ($scope.parts[i].mass / 2), ($scope.parts[i].mass / 2))
+            }
+            //new acceleration vals
+            $scope.parts[i].a = { x: 0, y: 0 };
+            var minDist = 0,
+            dist = 0;
+            for (var j = 0; j < $scope.parts.length; j++) {
+                if (j == i) {
+                    continue;
                 }
-                if (p.targ.x == Number.POSITIVE_INFINITY || p.targ.y == Number.POSITIVE_INFINITY) {
-                    alert('calc error: targ' + JSON.stringify(p.targ) + '\np.x: ' + JSON.stringify(p.x) + '\np.y: ' + JSON.stringify(p.y) +
-                        '\ncomparison targ: ' + JSON.stringify($scope.parts[i]) + '\nprev val: ' + JSON.stringify(prevVal))
+                minDist = $scope.parts[i].mass + $scope.parts[j].mass;
+                dist = Math.sqrt(Math.pow(Math.abs($scope.parts[i].x - $scope.parts[j].x), 2) + Math.pow(Math.abs($scope.parts[i].y - $scope.parts[j].y), 2));
+                if (dist <= minDist) {
+                    //distance too small (objects are 'touching'), which would lead to infinite distances
+                    continue;
                 }
+                //tan is op/adj is dy/dx
+                var rawA = ($scope.bigG * $scope.parts[j].mass) / Math.pow(dist, 2),
+                    tan = (($scope.parts[j].y - $scope.parts[i].y) / ($scope.parts[j].x - $scope.parts[i].x)); //tangent in radians
+                $scope.parts[i].a.x += rawA * Math.sin(Math.atan(tan));
+                $scope.parts[i].a.y += rawA * Math.cos(Math.atan(tan));
             }
-            //and mouse: 
-            if(p.x > $scope.mousePos.x && p.x!==$scope.mousePos.x && Math.abs(p.x-$scope.mousePos.x)>p.mass){
-                p.targ.x -= $scope.bigG * p.mass * 50 / Math.pow(Math.abs(p.x - $scope.mousePos.x), 2);
-            }else if(p.x!==$scope.mousePos.x && Math.abs(p.x-$scope.mousePos.x)>p.mass){
-                p.targ.x += $scope.bigG * p.mass * 50 / Math.pow(Math.abs(p.x - $scope.mousePos.x), 2);
+            //mouse!
+            dist = Math.sqrt(Math.pow(Math.abs($scope.parts[i].x - $scope.m.x), 2) + Math.pow(Math.abs($scope.parts[i].y - $scope.m.y), 2));
+            if (dist <= minDist) {
+                //distance too small (objects are 'touching'), which would lead to infinite distances
+                continue;
             }
-            if(p.y > $scope.mousePos.y && p.y!==$scope.mousePos.y && Math.abs(p.y-$scope.mousePos.y)>p.mass){
-                p.targ.y -= $scope.bigG * p.mass * 50 / Math.pow(Math.abs(p.y - $scope.mousePos.y), 2);
-            }else if(p.y!==$scope.mousePos.y && Math.abs(p.y-$scope.mousePos.y)>p.mass){
-                p.targ.y += $scope.bigG * p.mass * 50 / Math.pow(Math.abs(p.y - $scope.mousePos.y), 2);
-            }
-            // p.targ.x+=parseInt(p.x > $scope.mousePos.x ? -1 : 1)* $scope.bigG * p.mass * 50 / Math.pow(Math.abs(p.x - $scope.mousePos.x), 2)
-            // p.targ.y+=parseInt(p.y > $scope.mousePos.y ? -1 : 1)* $scope.bigG * p.mass * 50 / Math.pow(Math.abs(p.y - $scope.mousePos.y), 2)
-            //now, position changes
-            p.vx = (p.targ.x / p.mass)/.05;
-            p.vy = (p.targ.y / p.mass)/.05;
-            var maxRad = Math.sqrt(Math.pow(canv.width,2)+Math.pow(canv.height,2)),
-                hue = 60*Math.sqrt(Math.pow(Math.abs(p.vx),2)+Math.pow(Math.abs(p.vy),2))/maxRad;
-                val = 10+(50*Math.sqrt(Math.pow(Math.abs(p.vx),2)+Math.pow(Math.abs(p.vy),2))/maxRad);
+            var rawA = ($scope.bigG * $scope.m.m) / Math.pow(dist, 2),
+                tan = (($scope.parts[i].y - $scope.m.y) / ($scope.parts[i].x - $scope.m.x)); //tangent in radians
+            $scope.parts[i].a.x += rawA * Math.sin(Math.atan(tan));
+            $scope.parts[i].a.y += rawA * Math.cos(Math.atan(tan));
 
-            // console.log(n, hue, maxRad,'TARG:', p.targ, p.vx, p.vy)
-            console.log(p.vx,p.vy)
-
-            //finally, bounds stuff
-            if ((p.vx > 0 && p.x < (canv.width - 2)) || (p.vx < 0 && p.x > 0)) {
-                //within bounds
-                p.x += p.vx;
-            } else {
-                p.vx *= -1;
-                p.x += p.vx;
+            //end mouse
+            //adjust v as per a.
+            $scope.parts[i].vx += $scope.parts[i].a.x;
+            $scope.parts[i].vy += $scope.parts[i].a.y;
+            //adjust hue.
+            $scope.parts[i].temp.h = 60 * Math.min(Math.abs($scope.parts[i].vx), 4) / 4;
+            $scope.parts[i].temp.v = (80 * Math.min(Math.abs($scope.parts[i].vx), 4) / 4) + 10;
+            $scope.parts[i].data = { vx: $scope.parts[i].vx, vy: $scope.parts[i].vy, ax: $scope.parts[i].a.x, ay: $scope.parts[i].a.y, h: $scope.parts[i].temp.h };
+            //boundaries
+            //x
+            if (($scope.parts[i].x <= 0 && $scope.parts[i].vx < 0) || ($scope.parts[i].x >= $scope.field.x && $scope.parts[i].vx > 0)) {
+                $scope.parts[i].vx *= -1;
             }
-
-            if ((p.vy > 0 && p.y < (canv.height - 2)) || (p.vy < 0 && p.y > 0)) {
-                //within bounds
-                p.y += p.vy;
-            } else {
-                p.vy *= -1;
-                p.y += p.vy;
+            //y
+            if (($scope.parts[i].y <= 0 && $scope.parts[i].vy < 0) || ($scope.parts[i].y >= $scope.field.y && $scope.parts[i].vy > 0)) {
+                $scope.parts[i].vy *= -1;
             }
-            //done position changes!
-            ctx.fillStyle = 'hsl(' + ($scope.customHueMode ? $scope.customHue : hue) + ',100%,' + val + '%)';
-            ctx.fillRect(p.x, p.y, p.mass, p.mass);
-            if ($scope.glo) {
-                ctx.fillStyle = 'hsla(' + ($scope.customHueMode ? $scope.customHue : hue) + ',100%,' + val + '%,.1)';
-                ctx.fillRect(prevPos.x - (p.mass / 2), prevPos.y - (p.mass / 2), (p.mass * 2), (p.mass * 2));
+            //finally, move parts
+            $scope.parts[i].x += $scope.parts[i].vx;
+            $scope.parts[i].y += $scope.parts[i].vy;
+            //error detection!
+            if (isNaN($scope.parts[i].vx) || $scope.parts[i].vx >= Number.POSITIVE_INFINITY) {
+                throw new Error('vx error!')
             }
-            if ($scope.showVecs){
-                //tangent = o/a  = p.vy/p.vx;
-                //h=1 (unit vector!)
-                //sin(arctan(tan)) = o/h, o = hsin(arctan(tan));
-                //cos(arctan(tan)) = a/h, a = hsin(arctan(tan)); 
-                var theLine = {
-                    x:  10*Math.cos(Math.atan(p.vy/p.vx)),
-                    y: 10*Math.sin(Math.atan(p.vy/p.vx))
-                }
-                ctx.beginPath();
-                ctx.moveTo(p.x,p.y);
-                ctx.lineTo(p.x-theLine.x,p.y-theLine.y);
-                ctx.stroke();
+            if (isNaN($scope.parts[i].vy) || $scope.parts[i].vy >= Number.POSITIVE_INFINITY) {
+                throw new Error('vy error!')
             }
-            $scope.$digest();
-        })
-    }, 50);
-    canv.onmousemove=function(e){
-        $scope.mousePos.x = e.offsetX;
-        $scope.mousePos.y = e.offsetY;
-        console.log($scope.mousePos);
+            if (isNaN($scope.parts[i].a.x) || $scope.parts[i].a.x >= Number.POSITIVE_INFINITY) {
+                throw new Error('ax error!')
+            }
+            if (isNaN($scope.parts[i].a.y) || $scope.parts[i].a.y >= Number.POSITIVE_INFINITY) {
+                throw new Error('ay error!')
+            }
+            // console.log('I:',i,'VX',$scope.parts[i].vx,"VY", $scope.parts[i].vy)
+        }
+        $scope.$digest();
+    }, 50)
+    $scope.stopTimer = function() {
+        clearInterval($scope.timer)
+    };
+    $scope.m = {
+        x: 0,
+        y: 0,
+        m: 50
+    }
+    window.onmousemove = function(e) {
+        $scope.m.x = e.x || e.clientX;
+        $scope.m.y = e.y || e.clientY;
     }
 })
